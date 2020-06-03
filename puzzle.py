2b7f3714c0fe713419bb053f72263f10ab9bea1a
chrisbefin <christian@jba4.com>
2020-06-03 10:22:28 -0600

    switch to recursive and modular permutation method

--- puzzle.py
+++ puzzle.py
@@ -1,8 +1,11 @@
+#!usr/bin/env python
+
 import argparse
 from time import perf_counter
 import multiprocessing
 from multiprocessing.pool import Pool
 from multiprocessing import cpu_count
+import numpy as np
 
 class Puzzle_MP(object):
     """ This class handles all stages of solving the Vietnamese puzzle. It iterates over possible solutions, identifies solutions and then writes those solutions to an output file using a multiprocessing strategy. Solutions using arithmetic order of operations and sequential execution of operations are both identifed.
@@ -13,8 +16,8 @@ class Puzzle_MP(object):
     methods:
         init(): initializes data members and calls the class methods necessary to find and record all solutions
         run(): creates a process pool, provides tasks for the pool to execute and retrieves results of the executed tasks
-        pemdas(): finds solutions to the Vietnamese puzzle using standard arithmetic order of operations
-        sequential(): finds solutions to the Vietnamese puzzle using sequential execution of arithmetic operations
+        permutation(): recursive method which finds all permutations of a given list with constraints
+        find_solutions(): checks permutations provided by permutation() method for both sequential and PEMDAS solutions
         write(): writes all solutions to the given output file
     """
     def __init__(self, outputFile):
@@ -29,119 +32,71 @@ class Puzzle_MP(object):
         self.run() # find solutions using multiprocessing
         self.write() # write solutions to output file
 
+    def permutation(self, list, start, end, flag):
+        """ generates a list of all possible permutations of the input list with first digit in the inclusive range of start-end. When this method is first called, flag should be set to True
+            arguments: self, list to be permuted<list of ints>, start<int>, end<int>, flag<bool>
+            returns: list of int lists containing permutations
+        """
+        # base cases
+        if len(list) == 0: # empty list has no permutations
+            return []
+        if len(list) == 1: # one element has just one permutation
+            return [list]
+
+        results = [] # empty list that will store permutations
+
+        if flag == True: # flag is only True on first run
+            values = range(start-1,end) # limits the values for the first number in the permutation
+        else:
+            values = range(len(list)) # every subsequent recuvrsive run can choose from the full list of possibilities
+
+        for i in values: # iterate over possible values and recursively build up all possible permutations
+            m = list[i]
+            remList = list[:i] + list[i+1:] # remove list[i] from the list
+            for p in self.permutation(remList, start, end, False): # recursive call to permutation method
+                results.append([m] + p) # build all permutations with m as the first element
+        return results
+
     def run(self):
         """ creates a process pool, provides tasks for the pool to execute and retrieves results of the executed tasks
             arguments: self
             returns: None
         """
         processList = [] # list of spawned processes
-        with Pool(processes=self.processCount) as pool: # contextually create a process pool of size processCount
-            processList.append(pool.apply_async(self.pemdas, args=(1,5))) # create processes
-            processList.append(pool.apply_async(self.pemdas, args=(6,9)))
-            processList.append(pool.apply_async(self.sequential, args=(1,5)))
-            processList.append(pool.apply_async(self.sequential, args=(6,9)))
+        chunks = np.array_split(range(1,10), self.processCount) # divide work into even chunks
+        with Pool(processes=self.processCount) as pool: # create a process pool of size processCount
+            for chunk in chunks:
+                processList.append(pool.apply_async(self.find_solutions, args=(chunk[0],chunk[-1]))) # create a process for each chunk
+
             for process in processList: # for each process
                 outputValue = process.get() # retrieve return value from the executed process
                 self.solutions.extend(outputValue) # add to the solution list
 
-    def pemdas(self, start, end):
-        """ This function finds all PEMDAS solutions to the Vietnamese puzzle by iterating all possible numbers with the first number being in the range start-end. Upon completion, a summary is printed to terminal specifying the number of solutions found and the time taken.
+
+    def find_solutions(self, start, end):
+        """ This function finds all sequential and PEMDAS solutions to the Vietnamese puzzle by checking permutations provided by the permutation() method with the first number being in the range start-end. Upon completion, a summary is printed to terminal specifying the number of solutions found and the time taken.
             arguments:
                 start: <int> the beginning of the range of values for the first number that the function will evaluate
                 end: <int> the end of the range of values for the first number that the function will evaluate
-            returns: list of solutions <list>
+            returns: list of solutions <list of lists of ints>
         """
         time_start = perf_counter() # begin timer
-        starting_numbers = list(range(start,end+1))
-        solutions = [] # list of solutions
-        num_solutions = 0 # number of solutions discovered
-        total_checks = 0 # number of combinations checked
-
-        # the first value iterates over all possibilities specified by the start and end arguments
-        # each subsequent for loop represents the next variable in the puzzle
-        # since all variables must be unique integers, the options to choose from are reduced by 1 each loop until you arrive at the final loop, with only one possible number
-        for x2 in starting_numbers:
-            options = [1,2,3,4,5,6,7,8,9]
-            options.remove(x2) # reduce options by 1
-            x3_options = options.copy()
-            for x3 in x3_options:
-                x7_options = x3_options.copy()
-                x7_options.remove(x3) # reduce options by 1
-                for x7 in x7_options:
-                    x8_options = x7_options.copy()
-                    x8_options.remove(x7) # reduce options by 1
-                    for x8 in x8_options:
-                        x9_options = x8_options.copy()
-                        x9_options.remove(x8) # reduce options by 1
-                        for x9 in x9_options:
-                            x6_options = x9_options.copy()
-                            x6_options.remove(x9) # reduce options by 1
-                            for x6 in x6_options:
-                                x1_options = x6_options.copy()
-                                x1_options.remove(x6) # reduce options by 1
-                                for x1 in x1_options:
-                                    x4_options = x1_options.copy()
-                                    x4_options.remove(x1) # reduce options by 1
-                                    for x4 in x4_options:
-                                        x5_options = x4_options.copy()
-                                        x5_options.remove(x4) # reduce options by 1
-                                        for x5 in x5_options:
-                                            total_checks += 1 # increment number of combos checked
-                                            if (x1+13*x2/x3+x4+12*x5-x6-11+x7*x8/x9-10 == 66.0000): # check if this combo is a valid solution
-                                                num_solutions += 1 # increment number of discovered solutions
-                                                solutions.append(['O',x1,x2,x3,x4,x5,x6,x7,x8,x9]) # add the solution to the list of solutions
-        time_end = perf_counter()
-        print("{} checks performed and {} PEMDAS solutions found in {} seconds".format(total_checks, num_solutions, time_end-time_start)) # print summary to the terminal
-        return solutions
-
-    def sequential(self, start, end):
-        """ This function finds all sequential solutions to the Vietnamese puzzle by iterating all possible numbers with the first number being in the range start-end. Upon completion, a summary is printed to terminal specifying the number of solutions found and the time taken.
-            arguments:
-                start: <int> the beginning of the range of values for the first number that the function will evaluate
-                end: <int> the end of the range of values for the first number that the function will evaluate
-            returns: list of solutions <list>
-        """
-        time_start = perf_counter() # begin timer
-        starting_numbers = list(range(start,end+1))
         num_solutions = 0 # number of solutions
         total_checks = 0 # number of possibilities checked
         solutions = [] # list of solutions
+        numbers = [1,2,3,4,5,6,7,8,9] # list of numbers that will be permuted
+        options = self.permutation(numbers, start, end, True) # get permutations with contraints
+        for x1,x2,x3,x4,x5,x6,x7,x8,x9 in options:
+            total_checks += 1 # increment the number of checks performed
+            if (((((((((((((x1+13)*x2)/x3)+x4)+12)*x5)-x6)-11)+x7)*x8)/x9)-10) == 66.0000): # check to see if this solution is valid
+                num_solutions += 1 # increment the number of solutions
+                solutions.append(['S',x1,x2,x3,x4,x5,x6,x7,x8,x9]) # append valid solution to the list of solutions
+            if (x1+13*x2/x3+x4+12*x5-x6-11+x7*x8/x9-10 == 66.0000): # check if this combo is a valid solution
+                num_solutions += 1 # increment number of discovered solutions
+                solutions.append(['O',x1,x2,x3,x4,x5,x6,x7,x8,x9]) # add the solution to the list of solutions
 
-        # the first value iterates over all possibilities specified by the start and end arguments
-        # each subsequent for loop represents the next variable in the puzzle
-        # since all variables must be unique integers, the options to choose from are reduced by 1 each loop until you arrive at the final loop, with only one possible number
-        for x2 in starting_numbers:
-            options = [1,2,3,4,5,6,7,8,9]
-            options.remove(x2) # reduce options by 1
-            x3_options = options.copy()
-            for x3 in x3_options:
-                x7_options = x3_options.copy()
-                x7_options.remove(x3) # reduce options by 1
-                for x7 in x7_options:
-                    x8_options = x7_options.copy()
-                    x8_options.remove(x7) # reduce options by 1
-                    for x8 in x8_options:
-                        x9_options = x8_options.copy()
-                        x9_options.remove(x8) # reduce options by 1
-                        for x9 in x9_options:
-                            x6_options = x9_options.copy()
-                            x6_options.remove(x9) # reduce options by 1
-                            for x6 in x6_options:
-                                x1_options = x6_options.copy()
-                                x1_options.remove(x6) # reduce options by 1
-                                for x1 in x1_options:
-                                    x4_options = x1_options.copy()
-                                    x4_options.remove(x1) # reduce options by 1
-                                    for x4 in x4_options:
-                                        x5_options = x4_options.copy()
-                                        x5_options.remove(x4) # reduce options by 1
-                                        for x5 in x5_options:
-                                            total_checks += 1 # increment the number of checks performed
-                                            if (((((((((((((x1+13)*x2)/x3)+x4)+12)*x5)-x6)-11)+x7)*x8)/x9)-10) == 66.0000): # check to see if this solution is valid
-                                                num_solutions += 1 # increment the number of solutions
-                                                solutions.append(['S',x1,x2,x3,x4,x5,x6,x7,x8,x9]) # append valid solution to the list of solutions
         time_end = perf_counter() # time end
-        print("{} checks performed and {} sequential solutions found in {} seconds".format(total_checks, num_solutions, time_end-time_start)) # print summary to terminal
+        print("{} checks performed and {} total solutions found in {} seconds".format(total_checks, num_solutions, time_end-time_start)) # print summary to terminal
         return solutions
 
     def write(self):
